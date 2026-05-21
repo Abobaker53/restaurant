d3eb5e86b7b379eb90d94c4ef0641fd77e67af47
Abobakr53 <abobukerreds@gmail.com>
2026-05-21 21:15:46 +0300

    Added Icons

--- lib/core/app_manager/app_images.dart
+++ lib/core/app_manager/app_images.dart
@@ -3,4 +3,8 @@ abstract class AppImages {
   static String coffee="assets/images/coffee.png";
   static String chocolateBrownie="assets/images/Chocolate_Brownie.png";
   static String alejandrao="assets/images/alejandrao.png";
+  static String cardIcon="assets/icons/Card.svg";
+  static String deliverIcon="assets/icons/Deliver.svg";
+  static String documentIcon="assets/icons/Document.svg";
+  static String nextIcon="assets/icons/Next_icon_Arrow.svg";
 }
