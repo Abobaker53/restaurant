c355027877e4b1b3503d37641d7509c9baabcc31
Abobakr53 <abobukerreds@gmail.com>
2026-05-21 21:16:39 +0300

    Added color

--- lib/core/app_manager/app_colors.dart
+++ lib/core/app_manager/app_colors.dart
@@ -5,4 +5,5 @@ abstract class AppColors {
   static Color scaffoldBackground  = Color(0xffF5CB58);
   static Color textColor  = Color(0xff391713);
   static Color white  = Color(0xffF5F5F5);
+  static Color yello  = Color(0xffF3E9B5);
 }
