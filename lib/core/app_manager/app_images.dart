f1f69844e0701f94899c10d0314a502aca696b4e
Abobakr53 <abobukerreds@gmail.com>
2026-05-22 05:55:32 +0300

    new image

--- lib/core/app_manager/app_images.dart
+++ lib/core/app_manager/app_images.dart
@@ -7,4 +7,5 @@ abstract class AppImages {
   static String deliverIcon="assets/icons/Deliver.svg";
   static String documentIcon="assets/icons/Document.svg";
   static String nextIcon="assets/icons/Next_icon_Arrow.svg";
+  static String backIcon="assets/icons/Back_icon_Arrow.svg";
 }
